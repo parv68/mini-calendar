60b04c7f13277a0727a827a2aab455e8f3e1ca25
parv <147272703+parv68@users.noreply.github.com>
2024-07-17 09:20:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const monthNameEl = document.getElementById('month-name')
+const dayNameEl = document.getElementById('day-name')
+const dayNumEl = document.getElementById('day-number')
+const yearEl = document.getElementById('year')
+
+const date = new Date()
+const month = date.getMonth() 
+monthNameEl.innerHTML = date.toLocaleString('en', {month:"long"})
+dayNameEl.innerHTML = date.toLocaleString('en', {weekday:"long"})
+dayNumEl.innerHTML = date.getDate()
+yearEl.innerHTML = date.getFullYear()
